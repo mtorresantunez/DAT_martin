0b14b8e19eb4a02034ab31c94b84799d876fe7e5
Martin Torres <saiyan175@gmail.com>
2023-12-14 20:26:15 +0100

    practica hecha

--- websocket/servidor.py
+++ websocket/servidor.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
 from flask_sock import Sock
+import httpx
 
 app = Flask(__name__)
 sock = Sock(app)
+
+
+@app.route('/dogs')
+def dogs():
+    r = httpx.get('https://dog.ceo/api/breeds/image/random')
+    d = r.json()
+    return render_template('dogs.html', perrito=d.get('message'))
+
 
 
 @app.route('/')
